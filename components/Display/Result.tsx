470b9a74f935bce5ffb41eb5878cac62b34b347f
teddybear0430 <teddybear0430@hotmail.com>
2021-02-25 23:54:13 +0900

    0除算を行ってしまった場合を考慮。

--- components/Display/Result.tsx
+++ components/Display/Result.tsx
@@ -7,15 +7,28 @@ const ResultDiv = styled.div`
   font-size: 60px;
 `;
 
+// 0除算を行うとNaNや∞になってしまうので考慮する
+type fontSize = {
+  fontSize: number;
+};
+
+const SpecialCaseComponent = styled.div<fontSize>`
+  font-size: ${props => props.fontSize}px;
+`;
+
 interface Props {
   result: State;
 }
 
 // 大きい桁数の数値や小数点以下の桁数が入力された場合を考慮
 const fixDigits = (resultValue: string | number) => {
-  // 小数点が入力された時は文字列になっているので、除外する
-  if (typeof resultValue === 'string') {
-    return resultValue;
+  // 小数点が入力された時は文字列で返ってくるので分岐から除外する
+  if (typeof resultValue === 'string') return resultValue;
+
+  if (isNaN(resultValue)) {
+    return <SpecialCaseComponent fontSize={30}>0は除算できません</SpecialCaseComponent>;
+  } else if (resultValue === Infinity) {
+    return <SpecialCaseComponent fontSize={44}>Infinity</SpecialCaseComponent>;
   } else {
     const num = Math.round(resultValue * 1000000) / 1000000;
 
